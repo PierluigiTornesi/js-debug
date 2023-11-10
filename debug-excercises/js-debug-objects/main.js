c103ac8ee86ba1b1c52b025eaf3aadd26302e351
PierluigiTornesi <pierluigi.tornesi02@gmail.com>
2023-11-10 16:07:36 +0100

    add: correzione debug-objects

--- debug-excercises/js-debug-objects/main.js
+++ debug-excercises/js-debug-objects/main.js
@@ -50,37 +50,52 @@ const cars = [
     {
         manufacturer: 'Ford',
         model: 'Kuga',
-        type: 'Diesel'
+        type: 'diesel'
     },
     {
         manufacturer: 'Seat',
         model: 'Ibiza',
         type: 'metano'
-    }
+    },
     {
         manufacturer: 'Audi',
         model: 'R8',
-        type: 'Benzina'
+        type: 'benzina'
     },
 ];
+//il primo errore sta nella virgola mancante dopo la }
+//il quinto errore sta nella scrittura di Diesel, ha la d maiuscola
+//il sesto errore sta nella scrittura di Benzina, ha la B maiuscola
 
-const gasolineCars = cars.filter( (auto) >= auto.type === 'benzina');
 
+const gasolineCars = cars.filter( (auto) => auto.type === 'benzina');
+//il secondo errore sta nella scrittura di >=, vanno invertiti
 const dieselCars = cars.filter( (auto) => {
-    auto.type === 'diesel';
+    return auto.type === 'diesel';
 });
+//il terzo errore sta nella mancanza del return
 
 const otherCars = cars.filter( (auto) => {
-    return auto.type !== 'benzina' || auto.type !== 'diesel';
+    return auto.type !== 'benzina' && auto.type !== 'diesel';
 });
+//il quarto errore sta nell'|| , va messo un &&
 
+
+//questo codice restituisce in tre scritte differenti, tutte le auto di un certo tipo
 console.log('Auto a benzina');
 console.log('*******************************');
 console.log(gasolineCars);
+//in questo log verranno scritte tutte le auto a benzina
 
 console.log('Auto a diesel');
 console.log('*******************************');
 console.log(dieselCars);
+//in questo log verranno scritte tutte le auto a diesel
+
 
 console.log('Tutte le altre auto');
 console.log(otherCars);
+//in questo log verranno scritte tutte le altre auto diverse da quelle prima
+
+
+
